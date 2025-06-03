e408c31dd3d589709a65832e4700bc9cb127747c
bnznamco <gabriele.baldi.01@gmail.com>
2025-06-03 17:19:16 +0200

    feat: added error templates handling

--- packages/astro-camomilla-integration/src/utils/loadTemplate.ts
+++ packages/astro-camomilla-integration/src/utils/loadTemplate.ts
@@ -2,19 +2,20 @@ import type { AstroInstance } from "astro";
 import DefaultTemplate from "../templates/default.astro";
 
 export async function loadTemplate(
-  templateName: string | undefined,
+  templateName: string | string[] | undefined,
   template_map: Record<string, any>
 ) {
-  templateName = templateName || "";
-  templateName = templateName.replace(".html", "");
-  const Template = template_map[templateName];
-  if (Template) {
-    return Template as AstroInstance["default"];
-  } else {
-    console.warn(
-      `Could not load template ${templateName}` +
-        `,\nFalling back to default template @camomillacms/astro-integration/templates/default.astro`
-    );
-    return DefaultTemplate as AstroInstance["default"];
+  if (templateName && !Array.isArray(templateName)) {
+    templateName = [templateName];
   }
+  for (const name of templateName || []) {
+    if (name in template_map) {
+      return template_map[name] as AstroInstance["default"];
+    }
+  }
+  console.warn(
+    `Could not load template ${templateName}` +
+      `,\nFalling back to default template @camomillacms/astro-integration/templates/default.astro`
+  );
+  return DefaultTemplate as AstroInstance["default"];
 }
