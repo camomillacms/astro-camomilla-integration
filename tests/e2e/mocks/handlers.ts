4bb04c50aa98cd624a8c8382015f8a8668c2ea3d
bnznamco <gabriele.baldi.01@gmail.com>
2025-08-02 11:09:44 +0200

    tests: fix mock in e2e tests

--- tests/e2e/mocks/handlers.ts
+++ tests/e2e/mocks/handlers.ts
@@ -4,7 +4,8 @@ export const handlers = [
   http.get('http://localhost:8000/api/camomilla/pages-router/*', () => {
     return HttpResponse.json({
       template: 'defaults/pages/default',
-      template_file: 'defaults/pages/default'
+      template_file: 'defaults/pages/default',
+      is_public: true
     })
   }),
   http.get('http://localhost:8000/api/camomilla/users/current/', () => {
